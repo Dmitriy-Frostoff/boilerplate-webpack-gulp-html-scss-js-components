7948074506aea1e9089ef0b7c087e6baefb75920
dmitriy.frostoff <dmitriy.frostoff@yandex.ru>
2024-03-30 18:22:45 +0300

    chore: update gulp-cli package

--- configs/gulp/gulpfile.js
+++ configs/gulp/gulpfile.js
@@ -1,18 +1,18 @@
-import gulp from 'gulp';
+import gulp from "gulp";
 const { src, dest, series } = gulp;
-import include from 'gulp-file-include';
-import replace from 'gulp-replace';
-import rename from 'gulp-rename';
+import include from "gulp-file-include";
+import replace from "gulp-replace";
+import rename from "gulp-rename";
 
 export function html() {
-  return src('../../projectName/src/pages/index_gulp_include.html')
+  return src("../../projectName/src/pages/index_gulp_include.html")
     .pipe(
       include({
-        prefix: '@@',
+        prefix: "@@",
         indent: true,
-      }),
+      })
     )
     .pipe(replace(/="(\.\.\/){2,}/gi, '="../'))
-    .pipe(rename('index.html'))
-    .pipe(dest('../../projectName/src/pages/'));
+    .pipe(rename("index.html"))
+    .pipe(dest("../../projectName/src/pages/"));
 }
